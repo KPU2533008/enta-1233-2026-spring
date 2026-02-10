e14eb3906892daee61d37eec2600060ffd62f327
KPU2533008 <s2533008@kpuenta.ca>
2026-02-10 15:28:29 -0800

    Classwork 2/9

---
+++ Assets/_1233_StudentWork/Scripts/Entity/Target/PlayerTargetBehavior.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerTargetBehavior : MonoBehaviour, ITargetProvider {
+	[SerializeField] private Vector3 _offset = new(0, 1f, 0);
+
+	public bool HasTarget =>
+		PlayerService.Instance != null &&
+		PlayerService.Instance.GetPlayers().Length > 0 &&
+		PlayerService.Instance.GetPlayers()[0].Character != null;
+
+	public Transform GetTarget() {
+		if ( HasTarget )
+			return PlayerService.Instance.GetPlayers()[0].Character.transform;
+		return null;
+	}
+
+	public Vector3 GetTargetPosition() {
+		if ( HasTarget )
+			return PlayerService.Instance.GetPlayers()[0].Character.transform.position + _offset;
+		return transform.position;
+	}
+}
---
+++ Assets/_1233_StudentWork/Scripts/Entity/Weapon/ProjectileWeapon.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class ProjectileWeapon : MonoBehaviour, IWeapon {
+	[SerializeField] private Projectile _projectilePrefab;
+	[SerializeField] private Transform _muzzle;
+	[SerializeField] private float _fireRate = 1f;
+	[SerializeField] private float _arcHeight = 2f;
+
+	private float _nextFireTime;
+
+	public bool CanFire => Time.time >= _nextFireTime;
+
+	public void Fire(Quaternion direction) {
+		if ( !CanFire )
+			return;
+		_nextFireTime = Time.time + 1f / _fireRate;
+		SpawnProjectile(direction);
+	}
+
+	public void Fire(Vector3 targetPosition) {
+		// Direct fire by default, or could be configured for arc
+		Vector3 direction = ( targetPosition - _muzzle.position ).normalized;
+		Fire(Quaternion.LookRotation(direction));
+	}
+
+	public void FireArc(Vector3 targetPosition) {
+		if ( !CanFire )
+			return;
+		_nextFireTime = Time.time + 1f / _fireRate;
+		Vector3 velocity = CalculateArcVelocity(_muzzle.position, targetPosition, _arcHeight);
+		Projectile projectile = Instantiate(_projectilePrefab, _muzzle.position, _muzzle.rotation);
+		projectile.LaunchWithVelocity(velocity, gameObject);
+	}
+
+	private void SpawnProjectile(Quaternion direction) {
+		Projectile projectile = Instantiate(_projectilePrefab, _muzzle.position, direction);
+		projectile.Launch(direction * Vector3.forward, gameObject);
+	}
+
+	private Vector3 CalculateArcVelocity(Vector3 start, Vector3 end, float height) {
+		float displacementY = end.y - start.y;
+		Vector3 displacementXZ = new Vector3(end.x - start.x, 0, end.z - start.z);
+		float gravity = Physics.gravity.y;
+
+		float time =
+			Mathf.Sqrt(-2 * height / gravity) +
+			Mathf.Sqrt(2 * ( displacementY - height ) / gravity);
+
+		Vector3 velocityY = Vector3.up * Mathf.Sqrt(-2 * gravity * height);
+		Vector3 velocityXZ = displacementXZ / time;
+
+		return velocityXZ + velocityY * -Mathf.Sign(gravity);
+	}
+}
