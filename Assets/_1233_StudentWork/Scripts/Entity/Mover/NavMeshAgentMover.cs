38baa62064bcc172f28db225a9b17648e0cb4dfd
KPU2533008 <s2533008@kpuenta.ca>
2026-03-02 19:08:57 -0800

    Bugfix lab 5 + lab 7 (kinda)

--- Assets/_1233_StudentWork/Scripts/Entity/Mover/NavMeshAgentMover.cs
+++ Assets/_1233_StudentWork/Scripts/Entity/Mover/NavMeshAgentMover.cs
@@ -2,14 +2,19 @@
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
-public sealed class NavMeshAgentMover {
+public sealed class NavMeshAgentMover : MonoBehaviour {
 	[SerializeField] private NavMeshAgent _agent;
+	[SerializeField] private Vector3 _debugDestination;
 
 	public Vector3 Velocity => _agent.velocity;
 	public bool HasPath => _agent.hasPath;
 
 	public void SetDestination(Vector3 worldPos) {
 		_agent?.SetDestination(worldPos);
+	}
+
+	public void Start() {
+		SetDestination(_debugDestination);
 	}
 
 	public void Stop() {
